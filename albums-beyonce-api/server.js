67bb977af0eb3badb5a417be21c70ef6ab4b7a44
Ali Ashraf <adaydreamer10@gmail.com>
2021-06-03 01:50:18 +0200

    Get album by id using path params

---
+++ albums-beyonce-api/server.js
@@ -0,0 +1,49 @@
+const { query } = require('express');
+const express = require('express');
+const app = express();
+
+const albumsData = [
+    {
+      albumId: "10",
+      artistName: "Beyoncé",
+      collectionName: "Lemonade",
+      artworkUrl100:
+        "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
+      releaseDate: "2016-04-25T07:00:00Z",
+      primaryGenreName: "Pop",
+      url:
+        "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0",
+    },
+    {
+      albumId: "11",
+      artistName: "Beyoncé",
+      collectionName: "Dangerously In Love",
+      artworkUrl100:
+        "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
+      releaseDate: "2003-06-24T07:00:00Z",
+      primaryGenreName: "Pop",
+      url:
+        "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
+    },
+  ];
+  
+app.get("/albums", function (req, res) {
+  res.send(albumsData);
+});
+
+// Get - get album by id
+
+app.get("/albums/:albumId", (req, res) => {
+    const id = req.params.albumId
+    const album = albumsData.find((each)=>{
+        return each.albumId == id
+    })
+    if (album){
+        res.send(album)
+    } else {
+        res.status(404).send()
+    }
+    // res.send(album)
+});
+
+app.listen(3000, () => console.log("Server is up and running"))
